c128c1e5a4339aba2572f884abeff7d307ec2ab2
ri1700 <hazard3841@gmail.com>
2025-10-13 12:00:53 +0900

    ex01

--- javascript-practices/basics/ex01.js
+++ javascript-practices/basics/ex01.js
@@ -9,7 +9,8 @@ console.log("i:" + typeof(i)+ ":" + i);
 console.log("s:" + typeof(s)+ ":" + s);
 console.log("b:" + typeof(b)+ ":" + b);
 
-console.log("===== [02] 객체 타입, object type");
+
+console.log("===== [02-1] 객체 타입, object type");
 var i = new Number(10);
 var s = new String("hello world");
 var b = new Boolean("hello world");
@@ -17,9 +18,23 @@ var o = {};
 var a = [];
 var n = null;
 
+
 console.log("i:" + typeof(i)+ ":" + i);
 console.log("s:" + typeof(s)+ ":" + s);
 console.log("b:" + typeof(b)+ ":" + b); 
 console.log("o:" + typeof(o)+ ":" + o);
 console.log("a:" + typeof(a)+ ":" + a);
 console.log("n:" + typeof(n)+ ":" + n);
+
+console.log("===== [02-2] 객체 타입, function");
+function f1() {
+}
+
+var f2 = function() {
+}
+
+var f3 = new Function();
+
+console.log("f1:" + typeof(f1));
+console.log("f2:" + typeof(f2));
+console.log("f3:" + typeof(f3)); 
