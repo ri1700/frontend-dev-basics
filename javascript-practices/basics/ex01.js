573811eb17c082ec59f50b8ee12b18badccd7deb
ri1700 <hazard3841@gmail.com>
2025-10-13 11:06:10 +0900

    ex01

---
+++ javascript-practices/basics/ex01.js
@@ -0,0 +1,12 @@
+console.log("===== [01] 기본 타입, primimitive type");
+var u;
+var i = 10;
+var s = "hello";   
+var b = true;
+
+console.log("u:" + typeof(u));
+console.log("i:" + typeof(i));
+console.log("s:" + typeof(s));
+console.log("b:" + typeof(b));
+
+console.log("===== [02] 객체 타입, object type");
